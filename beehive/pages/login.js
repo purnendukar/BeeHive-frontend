bc75636a28af29500d8407a003ec6dd1a9c9411a
Purnendu Kar <purnendu.kar8@gmail.com>
2021-11-26 18:34:14 +0530

    chore: login page design

--- beehive/pages/login.js
+++ beehive/pages/login.js
@@ -1,15 +1,22 @@
+import Image from "next/image";
+
 import styles from "../styles/Login.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div className={styles.container_box}></div>
+      <div className={styles.container_box}>
+        <Image
+          src="/assets/images/communication.png"
+          alt="Login SignUp"
+          width={1200}
+          height={1000}
+        />
+      </div>
       <div className={`${styles.container_box} ${styles.login_section}`}>
         <div className={styles.login_form_section}>
           <div className={styles.welcome}>Welcome to BeeHive</div>
-          <div className={styles.description}>
-            Your Personal Project Manager
-          </div>
+          <div className={styles.info}>Your Personal Project Manager</div>
           <form className={styles.login_form}>
             <input
               type="text"
@@ -20,14 +27,14 @@ export default function Home() {
             <input
               type="password"
               autoComplete="password"
-              className="asdad ,asd sad as asd asd "
               placeholder="Password"
               required
             />
-            <button type="submit" className={styles.login_button}>
-              Login
-            </button>
+            <button type="submit">Login</button>
           </form>
+          <a href="/signup" className={styles.info}>
+            Don't have existing account?
+          </a>
         </div>
       </div>
     </div>
